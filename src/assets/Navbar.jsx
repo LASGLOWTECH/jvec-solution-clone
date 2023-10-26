cfe7bf7cbcd8b452f5c7f9f4a4b7a603bedc365a
LASGLOWTECH <omozemojeau@gmail.com>
2023-10-26 04:52:25 -0700

    made changes

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     const [show, setShow] = useState(false)
 
     const handleShow = () => {
-        setShow(show => !show)
+     setShow(prev=>!prev)
 
     }
 
@@ -21,12 +21,12 @@ export default function Navbar() {
     }
 
 
-    const navLinks = [{ link: 'Home', to: "/news" }, { link: 'About', to: "/news/latest" }, { link: 'Projeccts', to: "/" }, { link: 'Contact', to: "" },
+    const navLinks = [{ link: 'Home', to: "/" }, { link: 'Services', to: "/#services" }, { link: 'About', to: "/#Building-trust" }, { link: 'Contact', to: "/#contact" },
 
     ]
     return (
 <>
-        <div className=' bg-white fixed  top-0 width-full shadow    z-10  px-6 w-full md:px-24'>
+        <div className=' bg-white md:fixed  top-0 width-full shadow    z-10  px-6 w-full md:px-24'>
 
             <div className="  mx-auto flex  justify-between  w-full items-center   py-3  ">
 
@@ -62,28 +62,32 @@ export default function Navbar() {
 
                 </div>
 
-                {!show && < div className=' flex items-center    text-light justify-center md:hidden'>< RxHamburgerMenu className="font-bold text-lg  text-darkestBlue transition  ease-in duration-500 rounded-4xl w-8 h-8 " onClick={handleShow} /></div>}
-
-                {show && < div className=' flex items-center     justify-center md:hidden'><AiOutlineClose className="font-bold text-lg  text-darkestBlue transition duration-500  rounded-4xl  w-8 h-8 " onClick={handleClose} /></div>}
+                {!show ?  (< div className=' flex items-center    text-light justify-center md:hidden'>< RxHamburgerMenu className="font-bold text-lg  text-darkestBlue transition  ease-in duration-500 rounded-4xl w-8 h-8 " onClick={handleShow} /></div>) : ( < div className=' flex items-center     justify-center md:hidden'><AiOutlineClose className="font-bold text-lg  text-darkestBlue transition duration-500  rounded-4xl  w-8 h-8 " onClick={handleClose} /></div>)}
 
 
             </div>
 
 
-            
-            <div className='  hidden  md:hidden top-0 bg-white flex-col py-6  text-black  items-center'>
+
+
+
+
+
+        {show && (<div className=' bg-blue flex md:hidden transition-all  bg-white flex-col py-6 z-100  text-black  items-center'>
+                   
                     {navLinks.map((links) => {
                         return (
 
 
-                            <Link href={links.to} className='text-greyDark hover:border-b-2 py-6 z-50  font-bold tracking-wider text-base ' key={links} >{links.link}</Link>
+                            <Link href={links.to} className='text-greyDark hover:border-b-2 py-6 z-100  font-bold tracking-wider text-base ' onClick={handleClose}key={links} >{links.link}</Link>
 
 
                         )
                     })}
 
 
-                </div>
+                </div>)}
+            
 
 
 
