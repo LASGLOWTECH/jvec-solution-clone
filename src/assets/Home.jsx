86f1f1def254103f7feed0653873a667891c093f
LASGLOWTECH <omozemojeau@gmail.com>
2023-10-27 15:37:37 -0700

    added animation

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -118,7 +118,7 @@ export const Home = () => {
 
           {Services.map((service, index) => {
             return (
-              <div className='bg-blue-100 px-4 md:px-8 py-8 flex flex-col  items-center ' key={index}>
+              <div className='bg-blue-100 px-4 md:px-8 py-8 flex flex-col  items-center '  data-aos="slide-up"  key={index}>
                 <img src={service.Picture} alt='servive-image' className='w-[80px]' />
                 <h4 className='font-medium text-center py-4 text-[20px] px-10 md:px-16 text-black' >{service.Title}</h4>
 
@@ -129,10 +129,10 @@ export const Home = () => {
 
         </div>
 
-<div className='flex  justify-center items-center'>
-<a href ="/#Services"><button className='bg-blue-600 mx-auto px-6  mt-6 py-3 rounded-md  hover:bg-blue-500 transition duration-500 ease-in-out text-white   text-xl font-medium ' >Learn More</button></a>
+        <div className='flex  justify-center items-center'>
+          <a href="/#Services"><button className='bg-blue-600 mx-auto px-6  mt-6 py-3 rounded-md  hover:bg-blue-500 transition duration-500 ease-in-out text-white   text-xl font-medium ' >Learn More</button></a>
 
-</div>
+        </div>
 
 
 
@@ -142,51 +142,51 @@ export const Home = () => {
 
 
 
-{/* Our Stive section */}
+      {/* Our Stive section */}
       <section className='  px-6 py-6 md:p-24' id='about'>
 
-<div className='grid gap-2 py-6 mt-6 grid-cols-1 md:grid-cols-2'>
+        <div className='grid gap-2 py-6 mt-6 grid-cols-1 md:grid-cols-2'>
 
 
-<div className='flex max-w-md flex-col items-center justify-center py-4'>
+          <div className='flex max-w-md flex-col items-center justify-center py-4'>
 
-<h1 className='md:text-5xl text-3xl  text-center md:text-left   text-darkBlue  font-bold' data-aos="slide-down" duration="500">
-We thrive on Excellence
-</h1>
-<p className='text-grey-500 text-center  md:text-left font-medium pt-4 text-lg '>At JVEC, we’re Excellence driven. This mindset
+            <h1 className='md:text-5xl text-3xl  text-center md:text-left   text-darkBlue  font-bold' data-aos="slide-down" duration="500">
+              We thrive on Excellence
+            </h1>
+            <p className='text-grey-500 text-center  md:text-left font-medium pt-4 text-lg '>At JVEC, we’re Excellence driven. This mindset
 
-is responsible for the high-level commitment and dedication we give in delivering top-notch projects to our clients.
- Our goal is to help our clients stand  out from the crowd with exceptionally crafted products and services.</p>
+              is responsible for the high-level commitment and dedication we give in delivering top-notch projects to our clients.
+              Our goal is to help our clients stand  out from the crowd with exceptionally crafted products and services.</p>
 
-</div>
-
-
-<div className=''>
-<img src="synergy2.jpeg" alt='ourthrive-image ' className='rounded-3xl  hidden md:flex w-full' />
+          </div>
 
 
-<img src="synergy.jpeg" alt='ourthrive-image' className='md:-ml-[50px]  rounded-3xl  w-full md:-mt-[50px]' data-aos="slide-up" duration="500" />
-
-</div>
-
-
-</div>
+          <div className=''>
+            <img src="synergy2.jpeg" alt='ourthrive-image ' className='rounded-3xl  hidden md:flex w-full' />
 
 
+            <img src="synergy.jpeg" alt='ourthrive-image' className='md:-ml-[50px]  rounded-3xl  w-full md:-mt-[50px]' data-aos="slide-up" duration="500" />
+
+          </div>
+
+
+        </div>
 
 
 
-<h1 className='md:text-5xl text-3xl mt-24 text-darkBlue text-center font-bold'>
-Why Brands Choose Us
+
+
+        <h1 className='md:text-5xl text-3xl mt-24 text-darkBlue text-center font-bold'>
+          Why Brands Choose Us
         </h1>
         <p className='text-grey-500 text-center font-medium p-4 text-2xl '>we are outstanding</p>
 
 
         <div className='grid gap-5 py-3 mt-6 grid-cols-1 md:grid-cols-3'>
 
-          {Whyus.map((creative , index) => {
+          {Whyus.map((creative, index) => {
             return (
-              <div className=' px-4 py-4 flex flex-col  items-center 'data-aos="slide-up" duration="200"  key={index}>
+              <div className=' px-4 py-4 flex flex-col  items-center ' data-aos="slide-up" duration="200" key={index}>
                 <img src={creative.Picture} alt='servive-image' className='w-[100px]' />
                 <h3 className='font-bold  md:font-medium text-center py-4 text-2xl md:text-3xl   text-darkBlue' >{creative.Title}</h3>
                 <p className='text-grey-500 text-center  font-medium pt-2 text-lg '>{creative.Subtext}</p>
@@ -201,63 +201,63 @@ Why Brands Choose Us
 
 
 
-</section>
+      </section>
 
 
 
-{/* Building trust secton */}
-<section className='  px-6 py-6  md:px-24' id='Building-trust'>
+      {/* Building trust secton */}
+      <section className='  px-6 py-6  md:px-24' id='Building-trust'>
 
 
-<h1 className='md:text-5xl text-3xl mt-2 text-darkBlue text-center font-bold'>
-Building Trust
+        <h1 className='md:text-5xl text-3xl mt-2 text-darkBlue text-center font-bold'>
+          Building Trust
 
         </h1>
         <p className='text-grey-500 text-center font-medium p-4 text-2xl '>trust earned and preserved</p>
 
 
-<Testifiers/>
+        <Testifiers />
 
 
 
 
 
 
-<h1 className='md:text-5xl mt-24 text-2xl  text-darkBlue text-center font-bold'>
-They Trust Us
+        <h1 className='md:text-5xl mt-24 text-2xl  text-darkBlue text-center font-bold'>
+          They Trust Us
 
 
         </h1>
         <p className='text-grey-500 text-center font-medium p-4 text-2xl '>you can too</p>
 
 
-    
-
-</section>
-
-<section className='  px-6 py-6  md:px-24' id='Building-trust'>
-<Colab/>
 
 
+      </section>
+
+      <section className='  px-6 py-6  md:px-24' id='Building-trust'>
+        <Colab />
 
 
 
-<h1 className='md:text-5xl mt-24 text-3xl  text-darkBlue text-center font-bold'>
-How can we be of help?
+
+
+        <h1 className='md:text-5xl mt-24 text-3xl  text-darkBlue text-center font-bold'>
+          How can we be of help?
 
 
 
         </h1>
         <p className='text-grey-500 text-center font-medium p-4 text-2xl '>please shoot us a mail</p>
 
-</section>
+      </section>
 
 
-{/* contact section */}
-<section id="contact">
+      {/* contact section */}
+      <section id="contact">
 
-<Contact/>
-</section>
+        <Contact />
+      </section>
     </>
 
   )
